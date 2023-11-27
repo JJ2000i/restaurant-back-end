5efd91c18bcf2a933266743f554255fccd6cc74f
Jing Jin <145986880+JJ2000i@users.noreply.github.com>
2023-11-28 01:11:42 +0800

    connect MySQL, create menu and booking models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -78,6 +78,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    
+    'mydb': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemonData',   
+        'USER': 'LittleLemonAdmin',   
+        'PASSWORD': '',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306', 
     }
 }
 
